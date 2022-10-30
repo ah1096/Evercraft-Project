6b123404df917b15a5004a30f8b7c70d02afaf24
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-30 18:12:59 +0000

    nuked and paved (hence "fireball"); naming + alignment work

--- evercraft/evercraft.py
+++ evercraft/evercraft.py
@@ -1,67 +1,32 @@
-
 class Character:
 
-    SHEET = {
-        "name" : "D. Faulte",
-        "alignment" : {
-            "neutral" : True,
-            "good" : False,
-            "evil" : False,
-        },
-            #how do I return the human-readable alignment instead of just the index? XXXX
-            #changed to a nested dictionary to experiment with changing alignment???
-        "level": 1,
-        "AC" : 5,
-        "HP" : 10,
-        "abilityScores" : {
-            "STR" : 10, 
-            "DEX" : 10, 
-            "CON" : 10, 
-            "WIS" : 10, 
-            "INT" : 10, 
-            "CHA" : 10}
-            #changed abilityscores into a nested dictionary for easier readability
+    ABILITIES = {
+        "STR" : 10,
+        "CON" : 10,
+        "DEX" : 10,
+        "WIS" : 10,
+        "INT" : 10,
+        "CHA" : 10
     }
-    #how do i manipulate items in a dictionary? REFER TO W3 SCHOOLS
 
 
+#remove user inputs later?
+    def __init__(self, name = "Hugh Mann", alignment = "neutral"):
+        name = str(input("What is your name? "))
+        self.name = name
+
+        alignment = str.lower(input("Enter your alignment: Good, Neutral ,or Evil. "))
+        self.alignment = alignment
+
+        print (f"Your name is {self.name} and your alignment is {self.alignment}")
 
 
-    def __init__(self, name, alignment):
-        self.__name = name
+char1 = Character("Yllara")
+#print (f"Your name is {char1.name}, and your alignment is {char1.alignment}")
 
-    @property 
-    def name(self):
-        return self.__name
+#char2 = Character("Sirin", "good")
+#print (f"Your name is {char2.name}, and you are a {char2.alignment} character")
 
-    @name.setter
-    def name(self, value):
-        self.__name = value
-
-#######################################NAME TESTING#######
-#char1 = Character("Sirin")
-#print(char1.name)
-
-#char2 = Character("AAA")
-#char2.name = "Yllara"
-#print(char2.name)
-
-
-#####################################ALIGNMENT TESTING######
-#x = Character.SHEET.get("alignment")
-#returns the index of the chosen alignment
-#print("This is your alignment")
-#print (x)
-
-#Character.SHEET["alignment"] = input("Enter your alignment: ")
-#print (Character.SHEET.get("alignment"))
-
-#Character.SHEET.update({"alignment" : (input("What is your alignment? "))})
-#print(Character.SHEET.get("alignment"))
-
-############NESTED DICTIONARY METHOD TESTING######
-
-print(Character.SHEET["abilityScores"][STR])
 
 
 

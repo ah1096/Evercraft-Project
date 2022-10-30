274541259ef1a3d9fa99da18e073bc50a2ed4710
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-30 19:25:22 +0000

    adding attack function

--- evercraft/evercraft.py
+++ evercraft/evercraft.py
@@ -1,6 +1,6 @@
 class Character:
 
-    ABILITIES = {
+    AB_SCORES = {
         "STR" : 10,
         "CON" : 10,
         "DEX" : 10,
@@ -12,20 +12,42 @@ class Character:
 
 #remove user inputs later?
     def __init__(self, name = "Hugh Mann", alignment = "neutral"):
-        name = str(input("What is your name? "))
+        #name = str(input("What is your name? "))
         self.name = name
 
-        alignment = str.lower(input("Enter your alignment: Good, Neutral ,or Evil. "))
-        self.alignment = alignment
+        #alignment = str.lower(input("Enter your alignment: Good, Neutral ,or Evil. "))
+        self.alignment = str.lower(alignment)
+        #.lower makes alignment input look nicer when printed
 
         print (f"Your name is {self.name} and your alignment is {self.alignment}")
 
+        self.hp = 10
+        self.ac = 5
 
-char1 = Character("Yllara")
-#print (f"Your name is {char1.name}, and your alignment is {char1.alignment}")
+        self.dmg = 1
 
-#char2 = Character("Sirin", "good")
-#print (f"Your name is {char2.name}, and you are a {char2.alignment} character")
+
+
+        roll = int(input("Enter a number between 1 and 20: ")
+
+        def attack(self, ATKroll, foe):
+            if (ATKroll<20 and ATKroll >= foe.ac):
+                foe.hp == foe.hp - self.dmg
+            elif (ATKroll == 20):
+                foe.hp == foe.hp - (self.dmg * 2)
+            else:
+                print(f"Oops, your didn't hit {foe.name}")
+
+
+
+
+char1 = Character("Yllara", "good")
+print (char1.hp)
+
+foe = Character("Arally", "EvIl")
+print (foe.hp)
+
+
 
 
 
